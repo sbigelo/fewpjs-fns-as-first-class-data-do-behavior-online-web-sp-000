d16079983d43f05c954beb2964e0e3fb22f5b334
sbigelo <sean.bigelow1@gmail.com>
2021-04-01 12:06:51 -0400

    commit tree bf5b8db9c919d9d93ea49a765732932941b268ae

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(time) {
+  const times = time.split('\:').join('')
+  const parsed = parseInt(times)
+  if (parsed < 1200) {
+    return `Good Morning`;
+  }
+  else if (parsed >= 1200 && parsed <= 1700 ) {
+  return `Good Afternoon`;
+  }
+  else {
+    return `Good Evening`;
+  }
+}
+
+function displayMessage(arg) {
+  document.getElementById('greeting').innerText = arg;
+}
